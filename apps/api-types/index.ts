79d4c4165b586ce626029b5f2eb2c140c0742a51
aryanraj07 <aryanraj010010@gmail.com>
2026-03-18 23:50:48 +0530

    removed node modules

---
+++ apps/api-types/index.ts
@@ -0,0 +1,5 @@
+export type { AppRouter } from "../mini-ecommerce-backend/dist-types/server";
+export type {
+  RouterOutputs,
+  RouterInputs,
+} from "../mini-ecommerce-backend/dist-types/server/types";
