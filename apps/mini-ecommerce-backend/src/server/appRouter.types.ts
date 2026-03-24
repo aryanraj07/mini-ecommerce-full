43d1082ec7753c88f41b0c9cb946036fcf0f2da2
aryanraj07 <aryanraj010010@gmail.com>
2026-03-24 16:36:57 +0530

    fixed type error

--- apps/mini-ecommerce-backend/src/server/appRouter.types.ts
+++ apps/mini-ecommerce-backend/src/server/appRouter.types.ts
@@ -1,5 +1,5 @@
-import type { appRouter } from "./index.js";
+import type { AppRouter as _AppRouter } from "./appRouter.export.js";
 import type { inferRouterOutputs, inferRouterInputs } from "@trpc/server";
-export type AppRouter = typeof appRouter;
+export type AppRouter = _AppRouter;
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
 export type RouterInputs = inferRouterInputs<AppRouter>;
