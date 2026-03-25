454987f2f96ba5bb8c9b7f8baa377b3abfa169f2
aryanraj07 <aryanraj010010@gmail.com>
2026-03-25 14:42:11 +0530

    made changes

--- apps/mini-ecommerce-backend/src/helper/authHelper.ts
+++ apps/mini-ecommerce-backend/src/helper/authHelper.ts
@@ -1,5 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+
 import jwt from "jsonwebtoken";
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
